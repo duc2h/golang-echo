05a46f31cb3dc4cf4c1d48ce7686ebd2d655a538
HoangDuc <hoangduc02011998>
2020-01-09 21:09:26 +0700

    custome middleware

--- main.go
+++ main.go
@@ -9,13 +9,32 @@ import (
 	"github.com/hoangduc02011998/golang-echo/action"
 )
 
+/// custome middleware
+func serverHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set("server", "Edar/Ha")
+
+		return next(c)
+	}
+}
+
 func main() {
 
 	e := echo.New()
 
+	e.Use(serverHeader)
+
 	//g := e.Group("/admin")
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "[${time_rfc3339}]  ${status}  ${method}${path} ${latency_human}" + "\n",
+	}))
+
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "edar" && password == "123" {
+			return true, nil
+		}
+
+		return false, nil
 	}))
 
 	e.GET("/", func(c echo.Context) error {
